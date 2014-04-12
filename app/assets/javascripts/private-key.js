776b2136870e9fcae17032b9e7a8be35e720a5c1
Rebecca Meritz <rebecca@meritz.com>
2014-04-12 21:13:00 +0200

    File encyprted

--- app/assets/javascripts/private-key.js
+++ app/assets/javascripts/private-key.js
@@ -1,23 +1,29 @@
+/*global $, localStorage, FileReader, document, openpgp, Uint16Array */
 var key_form = $("form#private-key"),
-    file_form = $("form.new_upload");
+    file_form = $("form.new_upload"),
+    armored_private_key = localStorage.getItem("armored_private_key");
 
-function add_private_key() {
-  var private_key = $("#private-key-text").val();
-  localStorage.setItem("private_key", private_key);
+function add_armored_private_key() {
+  var armored_private_key = $("#private-key-text").val();
+  localStorage.setItem("armored_private_key", armored_private_key);
 }
 
 function handle_file(e) {
   var file = e.target.files[0],
       reader = new FileReader();
 
-  reader.onload = (function (the_file) {
-    console.log(the_file);
-  });
+  reader.onloadend = function(e) {
+    if (e.target.readyState == FileReader.DONE) {
+        var str = e.target.result,
+            private_keys = openpgp.key.readArmored(armored_private_key),
+            private_key = private_keys.keys[0];
+        private_key.decrypt();
+        console.log(openpgp.signClearMessage([private_key], str));
+    }
+  };
 
-  reader.readAsBinaryString(file);
-  //var key = openpgp.key.readArmored(private_key);
-  //openpgp.signClearMessage(key, "Burns");
+  reader.readAsText(file, "ascii");
 }
 
-key_form.on("submit", add_private_key);
+key_form.on("submit", add_armored_private_key);
 file_form.on("change", handle_file);
