ed718d624669e4a35d5b55c74cbcefabe92b7cad
Chad Mazzola <chad.mazzola@gmail.com>
2014-04-12 17:54:05 +0200

    Start to try to sign a file

--- app/assets/javascripts/private-key.js
+++ app/assets/javascripts/private-key.js
@@ -1,9 +1,23 @@
-var form = $("form#private-key");
+var key_form = $("form#private-key"),
+    file_form = $("form.new_upload");
 
 function add_private_key() {
   var private_key = $("#private-key-text").val();
   localStorage.setItem("private_key", private_key);
 }
 
-form.on("submit", add_private_key);
+function handle_file(e) {
+  var file = e.target.files[0],
+      reader = new FileReader();
 
+  reader.onload = (function (the_file) {
+    console.log(the_file);
+  });
+
+  reader.readAsBinaryString(file);
+  //var key = openpgp.key.readArmored(private_key);
+  //openpgp.signClearMessage(key, "Burns");
+}
+
+key_form.on("submit", add_private_key);
+file_form.on("change", handle_file);
