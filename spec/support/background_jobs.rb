dd13112ffec6cd367a21a423e28b0f4fa0debdb5
Mike Burns <mike@mike-burns.com>
2014-04-12 13:17:10 +0200

    Conditionally run the background jobs spec
    We removed `delayed_jobs` from the `Gemfile`, but we might add it back
    later, and really this should be conditional anyway. So we only run the
    background jobs spec if `delayed_jobs` is installed.

--- spec/support/background_jobs.rb
+++ spec/support/background_jobs.rb
@@ -1,19 +1,21 @@
-module BackgroundJobs
-  def run_background_jobs_immediately
-    delay_jobs = Delayed::Worker.delay_jobs
-    Delayed::Worker.delay_jobs = false
-    yield
-  ensure
-    Delayed::Worker.delay_jobs = delay_jobs
-  end
-end
-
-RSpec.configure do |config|
-  config.around(:each, type: :feature) do |example|
-    run_background_jobs_immediately do
-      example.run
+if defined? Delayed
+  module BackgroundJobs
+    def run_background_jobs_immediately
+      delay_jobs = Delayed::Worker.delay_jobs
+      Delayed::Worker.delay_jobs = false
+      yield
+    ensure
+      Delayed::Worker.delay_jobs = delay_jobs
     end
   end
 
-  config.include BackgroundJobs
+  RSpec.configure do |config|
+    config.around(:each, type: :feature) do |example|
+      run_background_jobs_immediately do
+        example.run
+      end
+    end
+
+    config.include BackgroundJobs
+  end
 end
